e1f7165247a0fa89cb242e243c533b0194556998
gnsamine <amine0gunes@gmail.com>
2026-04-21 19:04:46 +0300

    task2

--- task1/importantfunctions.py
+++ task1/importantfunctions.py
@@ -2,36 +2,50 @@ import cv2 as cv
 
 img= cv.imread("task1/photos/puma.png")
 cv.imshow("puma", img)
+print(img.shape)
 
 
 
 #convert the img to graysclae (intensity distributions of the image not the color)
-grayimg= cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+grayimg= cv.cvtColor(img, cv.COLOR_BGR2GRAY,cv.BORDER_CONSTANT,(0,0),0)
 cv.imshow("pumagray", grayimg)
 
+"""
+The kernels will define the size of the convolution, the weights applied to it,
+and an anchor point usually positioned at the center.
+
+"""
 #blur the image
-blur=cv.blur(img,(17,17), cv.BORDER_DEFAULT)
+blur=cv.blur(img,(17,17), cv.BORDER_CONSTANT,(0,0),0 )
 cv.imshow("blur", blur)
+print(blur.shape)
+
 
 #find edges
 edge= cv.Canny(grayimg,75,75)
-cv.imshow("edge", edge)
+
+#cv.imshow("edge", edge)
 #dilate 
 dilated=cv.dilate(edge,(-17,-17), iterations=3)
-cv.imshow("dilated", dilated)
+#cv.imshow("dilated", dilated)
 
 #erote the image
 eroted= cv.erode(dilated,(3,3), iterations=2)
-cv.imshow("eroded", eroted)
-cv.imwrite('edges.jpg', eroted)
+#cv.imshow("eroded", eroted)
+#cv.imwrite('edges.jpg', eroted)
 
 #resize
-resized= cv.resize(img, (400,400), cv.INTER_LINEAR)
+resized= cv.resize(img, (400,400), cv.INTER_AREA)
 cv.imshow("resized", resized)
 
 #crop 
 cropped= img[:  (img.shape[1]//2), (img.shape[1]//2):]
-cv.imshow("cropped", cropped)
+#cv.imshow("cropped", cropped)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
+
+"""
+
+
+"""
--- task1/test2.py
+++ task1/test2.py
@@ -12,38 +12,30 @@ Use masking or blending
 import cv2 as cv
 
 
-img= cv.imread("task1/photos/bardak.jpeg")
+img= cv.imread("task1/photos/library.jpeg")
 cv.imshow("img", img)
+blur=cv.GaussianBlur(img,(5,5),2)
+cv.imshow("new", img)
 
-gry= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-dilated= cv.dilate(gry,(3,3), iterations=3)
+gry= cv.cvtColor(blur,cv.COLOR_BGR2GRAY)
+edge=cv.Canny(gry,45,45)
 
-cv.imshow("gry", gry)
+dilated= cv.dilate(edge,(17,17), iterations=4)
 
+
+for i in range(img.shape[0]):
+    for j in range(img.shape[1]):
+        if dilated[i,j]!=0:
+            img[i,j,0]=250
+            img[i,j,1]=0
+            img[i,j,2]=250
+
+    
+cv.imshow("new", img)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-Task 6: Custom Filter
-Recreate blur manually using NumPy:
-Don’t use cv.blur
-Use averaging over kernel window
-👉 This builds real understanding of convolution
-"""
 
 
